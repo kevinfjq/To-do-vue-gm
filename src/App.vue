12e4625e019e11cd36204236a3599bd3226373d5
kevinfjq <kevifjq@gmail.com>
2025-05-24 15:36:18 -0300

    feat: add create task function

--- src/App.vue
+++ src/App.vue
@@ -5,24 +5,41 @@
     medium: 'Medium',
     high: 'High',
     urgent: 'Urgent',
-    critical: 'Critical'
+    critical: 'Critical',
   }
 
-  type Task = {
-    id: number;
-    text: string;
-    priority: keyof typeof priorities;
-    completed: boolean;
+  type Priority = keyof typeof priorities;
+
+  interface Task {
+    id: number,
+    text: string,
+    priority: Priority,
+    completed: boolean,
   }
 
   const newTaskText = ref('');
-  const tasks = ref([]);
+  const newTaskPriority = ref<Priority>('medium');
+  const tasks = ref<Task[]>([]);
   tasks.value = [
     { id: 1, text: 'Buy groceries', priority: 'medium', completed: false },
     { id: 2, text: 'Finish project report', priority: 'high', completed: false },
     { id: 3, text: 'Call mom', priority: 'low', completed: true },
     { id: 4, text: 'Schedule dentist appointment', priority: 'urgent', completed: false }
   ];
+
+  function addTask() {
+    if(newTaskText.value.trim() === '') return;
+
+    const newTask: Task = {
+      id: tasks.value.length + 1,
+      text: newTaskText.value,
+      priority: newTaskPriority.value,
+      completed: false,
+    }
+    tasks.value.push(newTask);
+    newTaskText.value = '';
+    newTaskPriority.value = 'medium';
+  }
 </script>
 
 <template>
@@ -31,6 +48,35 @@
   </header>
   <main>
     <section>
+      <form @submit.prevent="addTask">
+        <h2>Add a New Task</h2>
+        <div>
+          <label for="new-task-input">Task:</label>
+          <input
+            id="new-task-input"
+            type="text"
+            v-model="newTaskText"
+            placeholder="Task description"
+            required
+          >
+        </div>
+        <div>
+          <label for="new-task-priority">Priority:</label>
+          <select
+            id="new-task-priority"
+            v-model="newTaskPriority"
+          >
+            <option
+              v-for="(value, key) in priorities" :key="key" :value="key"
+            >
+              {{value}}
+            </option>
+          </select>
+        </div>
+        <button type="submit">Create task</button>
+
+
+      </form>
       <h2>Tasks</h2>
       <ul>
         <li v-for="task in tasks" :key="task.id">
